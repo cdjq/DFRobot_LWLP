88d0708f889b47729effc97ac29b27a7e25a16ec
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-08-06 16:35:14 +0800

    Update DFRobot_LWLP.h

--- DFRobot_LWLP.h
+++ DFRobot_LWLP.h
@@ -1,7 +1,6 @@
 /*!
  * @file DFRobot_LWLP.h
  * @brief Define the infrastructure of the DFRobot_LWLP class
- * @n This is a library of ...
  * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
  * @author [fengli](li.feng@dfrobot.com)
@@ -29,12 +28,12 @@
 #endif
 
 
-#define SENSOR_TYPE    LWPX  //传感器型号
-#define MAX_PRESURE   600 //量程的最大压力(Pa)
-#define MIN_PRESURE    600 //量程的最小压力(Pa)
+#define SENSOR_TYPE    LWPX  //Sensor type 
+#define MAX_PRESURE   600 //Maximum pressure range(Pa)
+#define MIN_PRESURE    600 //Minimum pressure range(Pa)
 
-#define MAX_TEMP    85.0 //量程的最大温度(℃)
-#define MIN_TEMP     40.0 //量程的最小温度(℃)
+#define MAX_TEMP    85.0 //Highest temperature range(℃)
+#define MIN_TEMP     40.0 //Lowest temperature range(℃)
 
 
 
@@ -46,25 +45,25 @@ public:
   #define ERR_IC_VERSION    -2      //Chip version does not match
   typedef struct
   {
-    uint8_t  status;/**<状态位>*/
-    uint8_t  presureDataH;/**<传感器原始值高字节>*/
-    uint8_t  presureDataM;/**<传感器原始值中间字节>*/
-    uint8_t  presureDataL;/**<传感器原始值低字节>*/
-    uint8_t  tempDataH;/**<温度原始值高字节>*/
-    uint8_t  tempDataM;/**<温度原始值中间字节>*/
-    uint8_t  tempDataL;/**<温度原始值低字节>*/
-    uint16_t  presureData;/**<合并后的原始压力值>*/
-    uint16_t  tempData;/**<合并后的温度原始值>*/
-    float  presure;/**<换算压力变量>*/
-    float  temperature;/**<换算温度变量>*/
-    float  presureFilter[5];    /**<滤波存储的压力原始值>*/
-    float  presureFilterMem;/**<滤波压力排序中间存储值>*/
-    double  presureFilterSum;/**<排序后累加>*/
-    float  presureFilterResult;/**<滤波后最终值>*/
-    float  temperatureFilter[5];    /**<滤波存储的压力原始值>*/
-    float  temperatureFilterMem;/**<滤波温度排序中间存储值>*/
-    double  temperatureFilterSum;/**<排序后累加>*/
-    float  temperatureFilterResult;/**<滤波后最终值>*/
+    uint8_t  status;/**<Status Bit>*/
+    uint8_t  presureDataH;/**<Sensor orignal value high byte>*/
+    uint8_t  presureDataM;/**<Sensor orignal value middle byte>*/
+    uint8_t  presureDataL;/**<Sensor orignal value low byte>*/
+    uint8_t  tempDataH;/**<Temperature orignal value high byte>*/
+    uint8_t  tempDataM;/**<Temperature orignal value middle byte>*/
+    uint8_t  tempDataL;/**<Temperature orignal value low byte>*/
+    uint16_t  presureData;/**<Combined pressure orignal value>*/
+    uint16_t  tempData;/**<Combined temperature orignal value>*/
+    float  presure;/**<Convert pressure variable>*/
+    float  temperature;/**<Convert temperature variable>*/
+    float  presureFilter[5];    /**<Pressure orignal value>*/
+    float  presureFilterMem;/**<Pressure sorting middle value>*/
+    double  presureFilterSum;/**<Sorting and accumulating>*/
+    float  presureFilterResult;/**<Final value processed by filter>*/
+    float  temperatureFilter[5];    /**<Temperature orignal value>*/
+    float  temperatureFilterMem;/**<Temperature sorting middle value>*/
+    double  temperatureFilterSum;/**<Sorting and accumulating>*/
+    float  temperatureFilterResult;/**<Final value processed by filter>*/
     float  presureDrift;
   }sLwlp_t ;
   
@@ -80,13 +79,13 @@ public:
    */
   int begin();
   /**
-   * @brief 获取单次测量的传感器数据
-   * @return 结构体类型的数据
+   * @brief Get sensor data of sigle measurement
+   * @return Data of struct type 
    */
   sLwlp_t getData(void);
   /**
-   * @brief 滤波处理函数,获取滤波处理的传感器数据
-   * @return 结构体类型的数据
+   * @brief Filter processing funcition, get sensor data processed by this function 
+   * @return Data of struct type 
    */
   sLwlp_t getfilterData(void);
   
@@ -94,7 +93,7 @@ private:
   sLwlp_t  lwlp;
   TwoWire *_pWire;
   uint8_t _address = 0x00;
-  void configChip(void);//配置芯片
+  void configChip(void);//Configure chip 
   /**
    * @brief Transport data to chip
    * @param Data address
